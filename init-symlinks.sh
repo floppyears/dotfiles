1a8e4dc6ee52bbf9a11e6d6e9841cd687290e794
Jose Cedeno <example@example.com>
2017-09-18 19:49:23 -0700

    Symlinks
    Set editor in gitconfig
    unset email

---
+++ init-symlinks.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.bashrc ~/.bashrc
+ln -s ~/dotfiles/.hushlogin ~/.hushlogin
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+
+
